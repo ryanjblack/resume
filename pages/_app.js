1aa1c8adfbfc2ea75a7a9ae8654401a20c40a6ea
Ryan Black <blackjosephryan@gmail.com>
2020-10-16 17:47:50 -0400

    gtags

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,20 @@
 import '../styles/index.css'
 
+import React, { Fragment } from 'react'
+import Router from 'next/router'
+
+import * as gtag from 'common/src/lib/gtag'
+
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
+
+export default ({ Component, pageProps }) => {
+  return (
+    <Fragment>
+      <Component {...pageProps} />
+    </Fragment>
+  )
 }
