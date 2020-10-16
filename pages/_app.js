466cadd0884caec1bb8f8a739e86d780dfa8c7de
Ryan Black <blackjosephryan@gmail.com>
2020-10-16 17:57:32 -0400

    gtags II

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,6 @@
 import '../styles/index.css'
 
-import React, { Fragment } from 'react'
-import Router from 'next/router'
-
-import * as gtag from 'common/src/lib/gtag'
-
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
-
-export default ({ Component, pageProps }) => {
-  return (
-    <Fragment>
-      <Component {...pageProps} />
-    </Fragment>
-  )
-}
--- pages/_document.js
+++ pages/_document.js
@@ -1,72 +1,21 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-
-import { Fragment } from 'react'
-import Document, { Head, Main, NextScript } from 'next/document'
-import FavIcon from '../assets/image/favicon.png'
-
-import { GA_TRACKING_ID } from '../lib/gtag'
+import Document, { Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
-}
-
-export default class CustomDocument extends Document {
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
-
-    const initialProps = await Document.getInitialProps(ctx)
-
-    // Check if in production
-    const isProduction = process.env.NODE_ENV === 'production'
-
-    return {
-      ...initialProps,
-      isProduction,
-    }
-  }
-
-  render() {
-    const { isProduction } = this.props
-
-    return (
       <html lang="en">
         <Head>
-          <link rel="shortcut icon" type="image/x-icon" href={FavIcon} />
-
-          {/* We only want to add the scripts if in production */}
-          {isProduction && (
-            <Fragment>
-              {/* Global Site Tag (gtag.js) - Google Analytics */}
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-              />
-              <script
-                dangerouslySetInnerHTML={{
-                  __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', '${GA_TRACKING_ID}', {
-                      page_path: window.location.pathname,
-                    });
-                  `,
-                }}
-              />
-            </Fragment>
-          )}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=${UA-180730486-1}"></script>
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', ${UA-180730486-1});`
+            }}
+          />
         </Head>
         <body>
           <Main />
