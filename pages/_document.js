a18e220c6891b22cce7d69a96adb8bd6e43d6dc4
Ryan Black <blackjosephryan@gmail.com>
2020-10-17 00:38:27 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,16 +5,15 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-180730486-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-180730486-1');
-</script>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N9ZPJL9');</script>
         </Head>
         <body>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N9ZPJL9"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
           <Main />
           <NextScript />
         </body>
