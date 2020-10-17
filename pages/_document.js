de4d0f3d97c441262377ad57342f36120d75d48b
Ryan Black <blackjosephryan@gmail.com>
2020-10-17 00:05:31 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,15 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
   render() {
     return (
-      <html lang="en">
-        <Head>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=${UA-180730486-1}"></script>
-          <script
-            async
-            dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', ${UA-180730486-1});`
-            }}
-          />
-        </Head>
+      <Html>
+        <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     )
   }
 }
