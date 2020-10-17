468cf6449295dd53ed42f4c6aee0e03830e67f51
Ryan Black <blackjosephryan@gmail.com>
2020-10-17 00:18:17 -0400

    dangerouslySetInnerHTML

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,6 @@
-import ReactGA from 'react-ga';
 import '../styles/index.css'
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-function initializeReactGA() {
-    ReactGA.initialize('UA-180730486-1');
-    ReactGA.pageview('/homepage');
-}
--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,23 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-180730486-1"
+          />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'UA-180730486-1');
+                `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
@@ -13,3 +29,5 @@ export default class MyDocument extends Document {
     )
   }
 }
+
+UA-180730486-1
